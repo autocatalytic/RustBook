75971f462c94038f8893b9fb64136f8a519586a1
Ken Leiserson <kenl09@gmail.com>
2022-03-14 14:48:19 -0700

    repeating integration tests, adding common module

--- 911.adder/src/lib.rs
+++ 911.adder/src/lib.rs
@@ -74,6 +74,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
--- 911.adder/tests/integration_test.rs
+++ 911.adder/tests/integration_test.rs
@@ -1,8 +1,16 @@
-use adder;
+use adder;  // each file in the *tests* directory is a 
+            // separate crate, so need to bring in
 
-mod common;
+mod common; // add common module to use it from any of the 
+            // integration test files as a module.
 
+// don't need to add cfg() here, as Cargo treats the tests 
+// directory specially and compiles files in this directory
+// only when we run 'cargo test'.
+//
 #[test]
 fn it_adds_to_six() {
+    common::setup();    // declared to improve test output
     assert_eq!(6, adder::add_two(4))
 }
+
