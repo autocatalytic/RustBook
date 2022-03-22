2c5aa59baa874673ca750cc5e147bd0f7e5a7cd3
Ken Leiserson <kenl09@gmail.com>
2022-03-22 12:17:05 -0700

    reworking object oriented portion for fun

---
+++ 971.objectorient/src/lib.rs
@@ -0,0 +1,77 @@
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>, // any type inside a Box that
+}                                       // implements the Draw trait
+
+// Whereas a generic type param can only be substituted with one concrete
+// type at a time, trait objects allow for multiple concrete types to fill
+// in for the trait object at runtime.
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// Note that using generics with trait bounds is more performant than
+// trait objects, if you know your collections are homogenous
+
+// Now let's implement some types of Draw traits. Note that a TextField
+// type may have additional fields like placeholder, but implements Draw
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+// Move to main.rs next
+
+
+
+
+
+// From first exercises around object oriented-style Rust code
+// 
+pub struct AveragedCollection {
+    pub list: Vec<i32>, // adding pub, just to test
+    pub average: f64,   // same, though it breaks the exercise goal ;-P
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();   // last vec value
+        match result {
+            // only removes last element of vec added
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
---
+++ 971.objectorient/src/main.rs
@@ -0,0 +1,29 @@
+
+use objectorient::{Draw, Button, Screen, AveragedCollection};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw select box
+    }
+}
+
+fn main() {
+
+    let mut collection: AveragedCollection = AveragedCollection {
+        list: vec!{1, 2, 3, 5},
+        average: 1.1,
+    };
+
+    collection.add(7);
+//    collection.remove(); 
+
+    println!("collection average: {:?}", collection.average);
+
+//    println!("Hello, world!");
+}
