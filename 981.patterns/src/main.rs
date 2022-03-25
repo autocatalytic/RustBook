730942b16af31213b93123fffb3a132bd383d442
Ken Leiserson <kenl09@gmail.com>
2022-03-25 16:00:15 -0700

    finishing up enums, moving to nested structs and ignores

--- 981.patterns/src/main.rs
+++ 981.patterns/src/main.rs
@@ -55,6 +55,29 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis at ({}, {})", x, y),
     }
+
+    // Destructuring Enums - using patterns to break down the variants
+    //
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x, y
+            );
+        }
+
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
+    }
+
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
@@ -64,5 +87,14 @@ fn print_coordinates(&(x, y): &(i32, i32)) {
 struct Point {
     x: i32,
     y: i32,
+}
+
+// Destructuring Enums
+//
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
