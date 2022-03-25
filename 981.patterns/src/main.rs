917f1a9254b8aa0d0323870e8f751baecdc324fb
Ken Leiserson <kenl09@gmail.com>
2022-03-24 18:06:09 -0700

    playing with different forms of matching

---
+++ 981.patterns/src/main.rs
@@ -0,0 +1,68 @@
+// Using patterns in conjunction with match() expressions and other 
+// constructs gives you more control over a program's control flow
+
+fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    println!("\n Destructuring +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+
+    let point = (3, 5);
+    print_coordinates(&point);
+
+    println!("\n structs +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+
+    let p = Point { x: 1, y: 7 };
+
+    let Point { x, y } = p;
+    assert_eq!(1, x);
+    assert_eq!(7, y);
+
+    // I like this method the best
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis at ({}, {})", x, y),
+    }
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
