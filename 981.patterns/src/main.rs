799de8a19c7a38439cf128b81e2f1a3971e2621e
Ken Leiserson <kenl09@gmail.com>
2022-03-26 15:28:58 -0700

    finishing up matching sections and ignoring variables

--- 981.patterns/src/main.rs
+++ 981.patterns/src/main.rs
@@ -58,12 +58,14 @@ fn main() {
 
     // Destructuring Enums - using patterns to break down the variants
     //
-    let msg = Message::ChangeColor(0, 160, 255);
+//    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
         Message::Quit => {
             println!("The Quit variant has no data to destructure.")
         }
+
         Message::Move { x, y } => {
             println!(
                 "Move in the x direction {} and in the y direction {}",
@@ -72,11 +74,29 @@ fn main() {
         }
 
         Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => println!(
+        Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
             "Change the color to red {}, green {}, and blue {}",
             r, g, b
         ),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println! (
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
     }
+
+    // @ Bindings
+    let msgb = MessageBind::Hello { id: 8 };
+
+    match msgb { 
+        MessageBind::Hello {
+            id: id_variable @ 3..=7, // 
+        } => println!("Found an id in range: {}", id_variable),
+        MessageBind::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        MessageBind::Hello { id } => println!("Found some other id: {}", id),
+     }
 
 }
 
@@ -89,12 +109,63 @@ struct Point {
     y: i32,
 }
 
-// Destructuring Enums
+// Destructuring Enums and nested structs
 //
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+//    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
+// Destructuring with patters is a convenient wy to use pieces of 
+// values, such as the value from each field in a struct, separately
+// from each other.
+
+// Ignoring Values in a Pattern
+//
+// Ignoring an entire value with _
+//
+// Ignoring parts of a value with a Nested _
+//
+//let mut setting_value = Some(5);
+//let new_setting_value = Some(10);
+//
+//match (setting_value, new setting_value) {
+//    (Some(_), Some(_)) => {
+//        println!("Cant overwrite an existing customized value");
+//    }
+//    _ => {
+//        setting_value = new_setting_value;
+//    }
+//}
+//
+//println!("Setting is {:?}", setting_value);
+//
+
+// Ignoring an unsused variable by starting its name with a _
+//
+// Extra conditionals with match guards
+//
+// match guards are additional if conditions specified after the pattern
+// in a match arm that must ALSO match, for that arm to be chosen. Like:
+//match num {
+//    Some(x) if x < 5 => println!("less than five: {}", x),
+//    Some(x) => println!("{}", x),
+//    None => (),
+//}
+
+// @ Bindings
+//
+// the at operator lets us create a variable that holds a value at the same
+// time we're testing that value to see whether it matches a pattern.
+//
+enum MessageBind {
+    Hello { id: i32 },
+}
